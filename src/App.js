4790866408b4bbd1cd65b8d5d3b82bce5c6e7675
rkesavai <rkesavai@amd.com>
2023-05-03 20:09:49 +0800

    beautiful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
-/** @format */
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Mymodal } from "./components/Mymodal";
+import { GlobalStyle } from "./globalStyles";
 
-export default function App() {
-  return <h1 className="text-4xl font-bold underline">Hello world!</h1>;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((showModal) => !showModal);
+  };
+
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Mymodal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </Container>
+    </>
+  );
 }
+
+export default App;
